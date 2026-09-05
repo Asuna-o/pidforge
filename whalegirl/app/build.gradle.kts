3c6e0439ba1b2c3ed598825252b79f3447ede40e
Asuna-o <vano20080428@outlook.com>
2026-09-05 11:18:29 +0800

    Update WhaleGirl assets and config for V0.6

--- whalegirl/app/build.gradle.kts
+++ whalegirl/app/build.gradle.kts
@@ -1,12 +1,14 @@
 plugins { id("com.android.application") }
+
 android {
     namespace = "com.whalegirl.pet"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.whalegirl.pet"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.1"
+        versionCode = 6
+        versionName = "0.6.0"
     }
 }
