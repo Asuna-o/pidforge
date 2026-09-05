5a95c0abd26deba351f84cf72cb1bca070413f4d
Asuna-o <vano20080428@outlook.com>
2026-09-05 10:49:24 +0800

    Add WhaleGirl app module build config

---
+++ whalegirl/app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "com.whalegirl.pet"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.whalegirl.pet"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "0.3.1"
+    }
+}
